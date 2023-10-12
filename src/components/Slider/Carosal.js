f76ba9727d9b9239b66792d29f9fb17efe9f7fb0
ayushhh1108 <ayushhirapara1108@gmail.com>
2023-10-13 02:13:57 +0530

    Add some sections of home page

---
+++ src/components/Slider/Carosal.js
@@ -0,0 +1,51 @@
+import React from "react";
+import slider1 from "../../assets/slider1.webp";
+import slider2 from "../../assets/slider2.webp";
+import "../../css/style.css";
+const Carousal = () => {
+  return (
+    <div
+      id="carouselExampleIndicators"
+      class="carousel slide w-75"
+      data-ride="carousel"
+    >
+      <ol class="carousel-indicators">
+        <li
+          data-target="#carouselExampleIndicators"
+          data-slide-to="0"
+          class="active"
+        ></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src={slider1} alt="First slide" />
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src={slider2} alt="Second slide" />
+        </div>
+      </div>
+      <a
+        class="carousel-control-prev"
+        href="#carouselExampleIndicators"
+        role="button"
+        data-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a
+        class="carousel-control-next"
+        href="#carouselExampleIndicators"
+        role="button"
+        data-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  );
+};
+
+export default Carousal;
